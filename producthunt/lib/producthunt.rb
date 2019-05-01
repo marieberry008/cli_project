4b0b647bd74d88e43eb5c597ea4c2a7ed9927cbc
Marie Berry (Leugermann) <marie@creativeos.io>
2019-05-01 20:05:25 +0000

    first commit

---
+++ producthunt/lib/producthunt.rb
@@ -0,0 +1,6 @@
+require "producthunt/version"
+
+module Producthunt
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ producthunt/lib/producthunt/version.rb
@@ -0,0 +1,3 @@
+module Producthunt
+  VERSION = "0.1.0"
+end
